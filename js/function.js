2d248c811e1192b13c06804f570265ac34894abc
Aline Iasmim <aline.isilvadamiao@gmail.com>
2021-11-15 20:44:57 -0300

    animação com scroll e seções

--- js/function.js
+++ js/function.js
@@ -1,12 +1,54 @@
+
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 // JAVASCRIPT DO SCROLL
-$('nav a').click(function(e){
+$('nav a').click(function (e) {
 	e.preventDefault();
 	var id = $(this).attr('href'),
 		targetOffset = $(id).offset().top,
-		menuHeigth =$('nav').innerHeight();
+		menuHeigth = $('nav').innerHeight();
 
 	$('html, body').animate({
 		scrollTop: targetOffset - menuHeigth
 	}, 500)
-		
+
 });
+
+// FUNÇÃO ANIMADA DAS SEÇÕES
+(function () {
+	var $target = $('.anime'),
+		animationClass = 'anime-start',
+		offset = $(window).height() * 3 / 4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function () {
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		})
+
+	}
+	animeScroll();
+	$(document).scroll(debounce (function() {
+		animeScroll();
+	},200));
+}());
+
